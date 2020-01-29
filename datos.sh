d84b689983bb1ae11dc283f7fa3ad7280c1feed4
luisakrac <50025904+luisakrac@users.noreply.github.com>
2020-01-29 17:48:53 -0600

    Add files via upload

---
+++ datos.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=ogIgg2cuknZpR3bwN1LjFmcr1SemlGdvB3Ugg2chJ2OkNGIg42bjBCdwlmcjNHIsVGIhRXdjVmalBSYy9GagEGIiAyboNWZKIXYlx2YK4Wai9iczV3LzVGbpZ2L4VXbyVGdu02bj9SY0FGZvEGdhR2Lgc2ZpZmbvN2LjFmcr1SemlGdvB3UvUWbvh2LzVGbpZ2L4VXbyVGdu02bj9SY0FGZvEGdhR2LgYXbKIXYlx2YKIXYlx2YKkXLgQXZsdWamBCbsFGdz5Wagc2awpgchVGbjpQetACdpdGIsxWY0NnbpByZrBnCKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
